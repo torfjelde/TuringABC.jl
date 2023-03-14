d53351ca8bd6b846e948610cfa211fc083da5ff5
Documenter.jl <documenter@juliadocs.github.io>
2023-03-14 10:05:28 +0000

    build based on 2121c0c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TuringABC","category":"page"},{"location":"#TuringABC","page":"Home","title":"TuringABC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TuringABC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TuringABC]","category":"page"}]
+}
